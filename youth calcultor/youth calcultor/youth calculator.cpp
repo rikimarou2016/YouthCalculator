eb71d233bb53914d81783992af11d152792dcff2
rikimarou2016 <46721251+rikimarou2016@users.noreply.github.com>
2019-01-22 01:39:15 +0000

    Add files via upload

---
+++ youth calcultor/youth calcultor/youth calculator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int age, ian = 43;
+	string name;
+	int difference = 43 - age;
+	
+
+	cout << "Please enter you age : ";
+	cin >> age;
+	cout << endl;
+	cout << "Please enter you name : ";
+	cin >> name;
+	cout << endl;
+	cout << name << " you are " << difference << "younger than Ian .";
+
+	system("pause");
+	cin.get();
+	return 0;
+
+
+
+
+
+
+
+
+
+
+}
